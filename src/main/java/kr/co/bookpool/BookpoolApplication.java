bbf76d9ef5e6563f92c6fe64c2fd7dcb52c590b2
Yongbum Kim <dt10002@naver.com>
2026-04-07 17:38:43 +0900

    init: project 초기 설정

---
+++ src/main/java/kr/co/bookpool/BookpoolApplication.java
@@ -0,0 +1,12 @@
+package kr.co.bookpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookpoolApplication.class, args);
+	}
+}
